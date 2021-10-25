11bd388e1db4e95d6926f71cba9dea7b1a5674f1
Ivan Gricyan <ivan_gricyan82@ukr.net>
2021-10-25 23:02:08 +0200

    feat: add position alert

--- js/items.js
+++ js/items.js
@@ -1,24 +1,31 @@
-
-
 for (let i = 1; i <= 10; i++) {
 
-const divItemElement = document.createElement("div");
-divItemElement.classList.add("item-element");
+  var divItemElement = document.createElement("div");
+  divItemElement.classList.add("item-element");
+  divItemElement.id = i;
 
-const divItemText = document.createElement("div");
-divItemText.classList.add("item-text");
+  const divItemText = document.createElement("div");
+  divItemText.classList.add("item-text");
 
-const itemHeader = document.createElement("h2");
-const itemHeaderText = document.createTextNode("Item");
+  const itemHeader = document.createElement("h2");
+  const itemHeaderText = document.createTextNode("Item");
 
-const itemPosition = document.createElement("p");
-const itemPositionText = document.createTextNode("Pos." + i);
+  const itemPosition = document.createElement("p");
+  const itemPositionText = document.createTextNode("Pos." + i);
 
-divItemElement.append(divItemText);
-divItemText.append(itemHeader);
-itemHeader.append(itemHeaderText);
-divItemText.append(itemPosition);
-itemPosition.append(itemPositionText);
+  divItemElement.append(divItemText);
+  divItemText.append(itemHeader);
+  itemHeader.append(itemHeaderText);
+  divItemText.append(itemPosition);
+  itemPosition.append(itemPositionText);
 
-document.querySelector(".item-container").append(divItemElement);
-}
+  document.querySelector(".item-container").append(divItemElement);
+
+  
+  const ItemButton = document.getElementById(i);
+
+  ItemButton.addEventListener("click", (event) => {
+    const ItemButtonNumber = ItemButton.getAttribute('id');
+    alert("Item's position: " + ItemButtonNumber);
+  });
+};
